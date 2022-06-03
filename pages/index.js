d17716d4304650409b05e1af2b7539631b1cb73d
buildtosuit <mr.jonbrown@gmail.com>
2022-06-03 11:59:43 -0700

    storefront

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import HeadComponent from '../components/Head';
 
 // Constants
-const TWITTER_HANDLE = "_buildspace";
+const TWITTER_HANDLE = "ilikeyourmath";
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
@@ -13,12 +13,12 @@ const App = () => {
       <HeadComponent/>
       <div className="container">
         <header className="header-container">
-          <p className="header"> 😳 Buildspace Emoji Store 😈</p>
-          <p className="sub-text">The only emoji store that accepts sh*tcoins</p>
+          <p className="header">   🖼 ILYM Virtual Gallery 🖼  </p>
+          <p className="sub-text">The official I Like Your Math marketplace</p>
         </header>
 
         <main>
-          <img src="https://media.giphy.com/media/eSwGh3YK54JKU/giphy.gif" alt="emoji" />
+          <img src="https://media.giphy.com/media/3o6ZtqyrkPXluOy5oc/giphy.gif" alt="emoji" />
         </main>
 
         <div className="footer-container">
@@ -28,7 +28,7 @@ const App = () => {
             href={TWITTER_LINK}
             target="_blank"
             rel="noreferrer"
-          >{`built on @${TWITTER_HANDLE}`}</a>
+          >{`find us @${TWITTER_HANDLE}`}</a>
         </div>
       </div>
     </div>
